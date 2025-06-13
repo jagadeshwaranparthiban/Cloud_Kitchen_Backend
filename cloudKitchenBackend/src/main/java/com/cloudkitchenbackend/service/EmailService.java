8fe2aeb50f92976435a2dcfd40cd6daf32ca3bab
jagadeshwaran <jagsartchannel@gmail.com>
2025-06-13 18:17:26 +0530

    refactor: Made EmailService functions asynchronous
    Signed-off-by: jagadeshwaran <jagsartchannel@gmail.com>

--- cloudKitchenBackend/src/main/java/com/cloudkitchenbackend/service/EmailService.java
+++ cloudKitchenBackend/src/main/java/com/cloudkitchenbackend/service/EmailService.java
@@ -3,9 +3,11 @@ package com.cloudkitchenbackend.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableAsync
 public class EmailService {
 
     private final String fromEmail="jagadeshrparthiban@gmail.com";
