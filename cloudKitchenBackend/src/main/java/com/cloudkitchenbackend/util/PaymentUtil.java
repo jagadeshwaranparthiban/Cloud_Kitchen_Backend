46747cf59a6e3f55a0d83faa2ec1c0cfda2303d3
jagadeshwaran <jagsartchannel@gmail.com>
2025-06-25 12:55:39 +0530

    Added: PaymentUtil and PaymentVerificationRequestDto
    Signed-off-by: jagadeshwaran <jagsartchannel@gmail.com>

---
+++ cloudKitchenBackend/src/main/java/com/cloudkitchenbackend/util/PaymentUtil.java
@@ -0,0 +1,31 @@
+package com.cloudkitchenbackend.util;
+
+import org.springframework.stereotype.Component;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Component
+public class PaymentUtil {
+
+    public static String hmacSha256(String secret, String data) throws Exception {
+        Mac mac = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        mac.init(secretKeySpec);
+        byte[] hash = mac.doFinal(data.getBytes(StandardCharsets.UTF_8));
+        return bytesToHex(hash);
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder(2 * bytes.length);
+        for (byte b : bytes) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1)
+                hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+}
