ee78ca6455acfc628ad7658e2885fd409c633aee
jagadeshwaran <jagsartchannel@gmail.com>
2025-12-11 14:19:21 +0530

    Added: Query to get weekly order data

--- cloudKitchenBackend/src/main/java/com/cloudkitchenbackend/repository/OrdersRepo.java
+++ cloudKitchenBackend/src/main/java/com/cloudkitchenbackend/repository/OrdersRepo.java
@@ -1,13 +1,24 @@
 package com.cloudkitchenbackend.repository;
 
+import com.cloudkitchenbackend.dto.WeeklyAnalyticsDto;
 import com.cloudkitchenbackend.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface OrdersRepo extends JpaRepository<Orders,Long> {
     Optional<Orders> findByOrderId(long orderId);
     void deleteByOrderId(long cancelOrderId);
+
+    @Query("SELECT COUNT(o.orderId) AS totalOrders, SUM(o.totalCost) AS totalRevenue " +
+            "FROM Orders o " +
+            "WHERE o.orderTime >= :dayStart AND o.orderTime < :dayEnd")
+    List<Object[]> findDailyAnalytics(LocalDateTime dayStart, LocalDateTime dayEnd);
 }
