9f2ad8fe1001f5cf7b936973ae07d93e29311e02
jagadeshwaran <jagsartchannel@gmail.com>
2025-12-12 23:13:42 +0530

    Added: ItemCategory Enum to classify items

---
+++ cloudKitchenBackend/src/main/java/com/cloudkitchenbackend/model/ItemCategory.java
@@ -0,0 +1,16 @@
+package com.cloudkitchenbackend.model;
+
+public enum ItemCategory {
+    STARTER,
+    MAIN_COURSE,
+    DESSERT,
+    BEVERAGE,
+    SNACKS,
+    SALAD,
+    SOUP,
+    ADD_ON,
+    PIZZA,
+    BURGER,
+    SANDWICH,
+    PASTA
+}
